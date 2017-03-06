f4609e5ac6f265792b00da74057982d6b822a5e3
MT <ztmenna@hotmail.com>
2017-03-06 14:10:53 -0600

    Update startMessage.js

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,22 +1,3 @@
 import chalk from "chalk";
 
 console.log(chalk.green('Starting app in dev mode ...'));
-
-
-/*
-SELECT *
-  FROM [MDM].[mdm].[VendorSourcing]
-
-DECLARE @CustomerID INT;
-
-	SELECT @CustomerID = P.PartyID
-    FROM   mdm.Party P (NOLOCK)
-    WHERE  P.PartyCode = 'ATC1' AND
-		   P.PartyTypeID = 3; -- Customer
-
-Select @CustomerID
-
-select * from mdm.Party where PartyTypeID = 3
-and PartyCode = 'ATC1'
---EXEC customer.ReadSourcingVendorSettings 'ATC1
-*/
